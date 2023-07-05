9a9a3510ccb1d3b707bee4a991a959bd8a65febf
chlwnstkd <jun1234755@naver.com>
2023-07-05 21:14:41 +0900

    2023-07-05

--- src/main/java/kopo/poly/service/UserInfoService.java
+++ src/main/java/kopo/poly/service/UserInfoService.java
@@ -2,9 +2,14 @@ package kopo.poly.service;
 
 import kopo.poly.dto.UserInfoDTO;
 import kopo.poly.persistance.mapper.IUserInfoMapper;
+import kopo.poly.util.CmmUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,22 +18,22 @@ public class UserInfoService implements IUserInfoService {
     private final IUserInfoMapper userInfoMapper;
 
     @Override
-    public UserInfoDTO getUserIdExists(UserInfoDTO pDTO) throws Exception {
-        return null;
-    }
-
-    @Override
-    public UserInfoDTO getEmailExists(UserInfoDTO pDTO) throws Exception {
-        return null;
-    }
-
-    @Override
     public int insertUserInfo(UserInfoDTO pDTO) throws Exception {
         log.info(this.getClass().getName() + ".insertUserInfo Start!");
 
-        int res = 0;
+//        int res = 0;
+//
+//        res = userInfoMapper.insertUserInfo(pDTO);
 
-        res = userInfoMapper.insertUserInfo(pDTO);
+        Optional<UserInfoDTO> rDTO = Optional.ofNullable(
+                userInfoMapper.getUserIdExists(pDTO));
+
+        int res = 0;
+        if(!rDTO.isPresent()) {
+            res = userInfoMapper.insertUserInfo(pDTO);
+        } else {
+            res = 2;
+        }
 
         log.info(this.getClass().getName() + ".insertUserInfo End!");
 
@@ -37,16 +42,20 @@ public class UserInfoService implements IUserInfoService {
 
     @Override
     public UserInfoDTO getLogin(UserInfoDTO pDTO) throws Exception {
-        return null;
+        log.info(this.getClass().getName() + ".getLogin Start!");
+        UserInfoDTO rDTO = Optional.ofNullable(userInfoMapper.getLogin(pDTO)).orElseGet(UserInfoDTO::new);
+        //위 코딩은 코드가 줄어들지만 가독성이 떨어진다
+        //그래서 가독성을 위해 아래와 같이 코딩할 수도 있다
+        //UserInfoDTO rDTO = userInfoMapper.getLogin(pDTO);
+        //if(rDTO == null)
+        //  rDTO = new UserInfoDTO();
+        if (CmmUtil.nvl(rDTO.getUser_id()).length() > 0) {
+            log.info("로그인 성공");
+        }
+        //Cmmutil.nvl은 들어온 값이 null이면 빈 문장으로 바꾼다. 아마도 ""로 바뀌는 듯
+        //그래서 길이도 0
+        log.info(this.getClass().getName() + ".getLogin End!");
+        return rDTO;
     }
 
-    @Override
-    public UserInfoDTO searchUserIdOrPasswordPrac(UserInfoDTO pDTO) throws Exception {
-        return null;
-    }
-
-    @Override
-    public int newPasswordProc(UserInfoDTO pDTO) throws Exception {
-        return 0;
-    }
 }
